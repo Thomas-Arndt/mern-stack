8caf17a5da04209ae2c3328191156a14f564230c
Thomas Arndt <arndtt42@gmail.com>
2022-01-08 15:51:06 -0800

    Assignment to utilize create-rect-app and jsx in a react web application

---
+++ react/intro_to_react/first-jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-start">Hello Dojo!</h1>
+      <h2 class="text-start">Things I need to do:</h2>
+      <ul>
+        <li class="text-start">Learn React</li>
+        <li class="text-start">Climb Mt. Everest</li>
+        <li class="text-start">Run a marathon</li>
+        <li class="text-start">Feed the cats</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
